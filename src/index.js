03fea3e566d51061a8ec1726388ea80e1c536979
Dylan Palmboom <nofxboy1234@gmail.com>
2023-12-21 21:21:02 +0200

    Write a simple module and include it in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
+import myName from './myName';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.textContent = myName('Cody');
 
   return element;
 }
