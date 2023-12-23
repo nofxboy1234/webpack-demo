2f4a51ea1efd6ed3343bde86f633deb1fb841c65
Dylan Palmboom <nofxboy1234@gmail.com>
2023-12-24 00:18:58 +0200

    bundle css

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import _ from 'lodash';
-import myName from './myName';
-import { myFunction, myVariable, MyClass } from './parentModule';
+import './style.css';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
-  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.textContent = myName('Cody');
-  myFunction();
-
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+  
   return element;
 }
 
