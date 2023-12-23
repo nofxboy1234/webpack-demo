86710223cb98608e0fa5f604fd1ecd04f2f02d55
Dylan Palmboom <nofxboy1234@gmail.com>
2023-12-23 09:42:22 +0200

    Use myFunction from parentModule inside index.js module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import myName from './myName';
+import { myFunction, myVariable, MyClass } from './parentModule';
 
 function component() {
   const element = document.createElement('div');
@@ -7,6 +8,7 @@ function component() {
   // Lodash, currently included via a script, is required for this line to work
   // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.textContent = myName('Cody');
+  myFunction();
 
   return element;
 }
