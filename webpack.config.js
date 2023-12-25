701db074df3c4f01716816059fae3a6fe9e3f864
Dylan Palmboom <nofxboy1234@gmail.com>
2023-12-25 19:48:02 +0200

    Generate multiple bundles by specifying them in the config

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,25 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production',
-  entry: './src/index.js',
+  // entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
   output: {
-    filename: 'bundle.js',
+    // filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      // title: 'Webpack 5 Video Tutorials',
-      // filename: 'index.html',
-      // inject: 'head',
-      // scriptLoading: 'defer',
-      template: './src/template.html',
-    }),
+    // new HtmlWebpackPlugin({
+    //   // title: 'Webpack 5 Video Tutorials',
+    //   // filename: 'index.html',
+    //   // inject: 'head',
+    //   // scriptLoading: 'defer',
+    //   template: './src/template.html',
+    // }),
   ],
 };
